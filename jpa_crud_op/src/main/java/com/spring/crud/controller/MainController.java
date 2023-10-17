e95b5fb5718cec88d7323e2e91b706fc54a46968
Kishore1K <reddykishore527@gmail.com>
2023-10-17 21:34:34 +0530

    spring dev tools

--- jpa_crud_op/src/main/java/com/spring/crud/controller/MainController.java
+++ jpa_crud_op/src/main/java/com/spring/crud/controller/MainController.java
@@ -2,6 +2,7 @@ package com.spring.crud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -10,4 +11,37 @@ public class MainController {
     public  void home(){
         System.out.println("MainController.home");
     }
+
+
+    @RequestMapping("/about")
+    @ResponseBody
+    public  String about(){
+        return "About";
+    }
+
+    @RequestMapping("/contact")
+    @ResponseBody
+    public  String contact(){
+        return "ContactPage";
+    }
+
+
+    @RequestMapping("/link")
+    @ResponseBody
+    public  String link(){
+        return "link";
+    }
+
+    @RequestMapping("/link1")
+    @ResponseBody
+    public  String link1(){
+        return "link1";
+    }
+
+    @RequestMapping("/mail")
+    @ResponseBody
+    public  String mail(){
+        return  "Mail Page";
+    }
+
 }
