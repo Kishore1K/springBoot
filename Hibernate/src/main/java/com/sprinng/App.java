b8942a126de94d064aca30a3d9487284ae095306
Kishore1K <reddykishore527@gmail.com>
2023-10-19 17:39:52 +0530

    Hibernate properties

--- Hibernate/src/main/java/com/sprinng/App.java
+++ Hibernate/src/main/java/com/sprinng/App.java
@@ -5,13 +5,18 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
         Configuration cfgConfiguration = new Configuration();
@@ -26,16 +31,32 @@ public class App
         
         
         	Student student = new Student();
-        	student.setCity("ch");
-        	student.setId(2);
-        	student.setName("manju");
+        	student.setCity("KGF");
+        	student.setId(4);
+        	student.setName("abi");
         	
         	System.out.println(student);
+
+//			Image
+		FileInputStream fio = new FileInputStream("D:\\JAVA\\JAVAEE\\springBoot\\Hibernate\\src\\main\\resources\\resume_preview.png");
+		byte[] data = new byte[fio.available()];
+		fio.read(data);
+
+//			Address Table
+		Address address = new Address();
+		address.setCity("KGF");
+		address.setStreet("1st cross");
+		address.setOpen(true);
+		address.setAddedDate(new Date());
+		address.setX(12.455);
+		address.setImage(data);
+		System.out.println("address = " + address);
         	
         	Session session = factory.openSession();
 //        	session.beginTransaction();
         	Transaction tx = session.beginTransaction();
         	session.save(student);
+			session.save(address);
 //        	session.getTransaction().commit();
         	tx.commit();
         	
