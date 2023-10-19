eea7f2385c10a2cfc2469adbcea70d08511c8aec
Kishore1K <reddykishore527@gmail.com>
2023-10-19 16:37:14 +0530

    session factory config

--- Hibernate/src/main/java/com/sprinng/App.java
+++ Hibernate/src/main/java/com/sprinng/App.java
@@ -1,5 +1,8 @@
 package com.sprinng;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Configuration cfgConfiguration = new Configuration();
+        cfgConfiguration.configure("hibernate.cfg.xml");
+        
+        SessionFactory factory = cfgConfiguration.buildSessionFactory();
+//    or this     SessionFactory  factory =  new Configuration().configure().buildSessionFactory();
+        
+        System.out.println(factory);
     }
 }
