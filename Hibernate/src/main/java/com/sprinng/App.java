2498bd029c956fe3b76edf839b5576fde8eec148
Kishore1K <reddykishore527@gmail.com>
2023-10-19 17:00:40 +0530

    Inial Table Creaction

--- Hibernate/src/main/java/com/sprinng/App.java
+++ Hibernate/src/main/java/com/sprinng/App.java
@@ -1,6 +1,8 @@
 package com.sprinng;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -18,6 +20,25 @@ public class App
         SessionFactory factory = cfgConfiguration.buildSessionFactory();
 //    or this     SessionFactory  factory =  new Configuration().configure().buildSessionFactory();
         
+        
+        
         System.out.println(factory);
+        
+        
+        	Student student = new Student();
+        	student.setCity("ch");
+        	student.setId(2);
+        	student.setName("manju");
+        	
+        	System.out.println(student);
+        	
+        	Session session = factory.openSession();
+//        	session.beginTransaction();
+        	Transaction tx = session.beginTransaction();
+        	session.save(student);
+//        	session.getTransaction().commit();
+        	tx.commit();
+        	
+        	session.close();
     }
 }
