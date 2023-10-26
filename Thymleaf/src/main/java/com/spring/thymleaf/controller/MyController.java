6cc8afc40986cec002625adfa7ff4290303d7f96
Kishore1K <reddykishore527@gmail.com>
2023-10-26 19:37:00 +0530

    Thymleaf Intro

--- Thymleaf/src/main/java/com/spring/thymleaf/controller/MyController.java
+++ Thymleaf/src/main/java/com/spring/thymleaf/controller/MyController.java
@@ -1,4 +1,20 @@
 package com.spring.thymleaf.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@Controller
 public class MyController {
+
+    @GetMapping("/about")
+    public  String about(Model model){
+        System.out.println("MyController.about");
+        model.addAttribute("name", "Kishore");
+        model.addAttribute("date", new Date().toString());
+        return  "about";
+    }
 }
