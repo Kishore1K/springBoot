3715805f7c333fff4795750f49ce8340c60580fe
Kishore1K <reddykishore527@gmail.com>
2023-10-27 18:42:15 +0530

    Iteration th:each: status, odd, even , index, first, last

--- Thymleaf/src/main/java/com/spring/thymleaf/controller/MyController.java
+++ Thymleaf/src/main/java/com/spring/thymleaf/controller/MyController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class MyController {
@@ -13,9 +14,17 @@ public class MyController {
     @GetMapping("/about")
     public  String about(Model model){
         System.out.println("MyController.about");
-
         model.addAttribute("name", "Kishore");
         model.addAttribute("date", new Date().toString());
         return  "about";
+    }
+
+
+//    handling for iteration
+    @GetMapping("/loop")
+    public  String iterateHandler(Model m){
+        List<String> names= List.of("Kishore", "Reddy", "Karthick","Ankush", "Manju");
+        m.addAttribute("names", names);
+        return  "iterate";
     }
 }
