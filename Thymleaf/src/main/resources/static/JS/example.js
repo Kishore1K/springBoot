06d42683f95f777b87aedd2cf79588bf077713ec
Kishore1K <reddykishore527@gmail.com>
2023-11-01 19:20:24 +0530

    Adding Static Files to Thymeleaf

--- Thymleaf/src/main/resources/static/JS/example.js
+++ Thymleaf/src/main/resources/static/JS/example.js
@@ -3,8 +3,9 @@ console.log("Hello World")
 alert("Hi This is a JS File")
 
 name = prompt("Enter the Name")
-alert("Hello "+name)
+// alert("Hello "+name)
 document.addEventListener("click", ()=>{
     alert("Hello"+name)
-    document.getElementById("#name").innerText=name;
+    let n=document.getElementById("#name");
+    n.innerText=name
 });
